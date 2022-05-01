f88a05da2adede420e7013d67676e0a3ee302151
Claudius <claude-1991@hotmail.com>
2022-05-01 12:38:48 +0200

    Initial Commit

---
+++ CompositionalLayout/CompositionalLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionalLayoutApp.swift
+//  CompositionalLayout
+//
+//  Created by Claudius Kockelmann on 01.05.22.
+//
+
+import SwiftUI
+
+@main
+struct CompositionalLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
