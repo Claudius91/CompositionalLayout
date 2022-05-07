f88fdddd76217beb2bbd92e60ea0b0c935100301
Claudius <claude-1991@hotmail.com>
2022-05-07 18:25:16 +0200

    Added CustomTabbar

--- CompositionalLayout/ContentView.swift
+++ CompositionalLayout/ContentView.swift
@@ -9,54 +9,78 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 struct ContentView: View {
+    // MARK: Hiding Native One
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
     @StateObject var imageFetcher: ImageFetcher = .init()
     
+    @State var currentTab: Tab = .bookmark
+    
     var body: some View {
-        NavigationView {
-            Group {
-                // MARK: Custom View
-                if let images = imageFetcher.fetchedImages{
-                    ScrollView{
-                        CompositionalView(items: images, id: \.id) { item in
-    //                        ZStack {
-    //                            Rectangle()
-    //                                .fill(.cyan)
-    //                            Text("\(item)")
-    //                                .font(.title.bold())
-    //                        }
-                            GeometryReader{ proxy in
-                                let size = proxy.size
-                                WebImage(url: URL(string: item.download_url))
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: size.width, height: size.height)
-                                    .cornerRadius(10)
-                                    .onAppear {
-                                        if images.last?.id == item.id {
-                                            imageFetcher.startPagination = true
-                                        }
-                                    }
-                            }
-                        }
-                        .padding()
-                        .padding(.bottom, 10)
-                        
-                        if imageFetcher.startPagination && !imageFetcher.endPagination {
-                            ProgressView()
-                                .offset(y: -15)
-                                .onAppear{
-                                    // MARK: Slight Delay
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                                        imageFetcher.updateImages()
+        VStack(spacing: 0) {
+            TabView(selection: $currentTab) {
+                NavigationView {
+                    Group {
+                        // MARK: Custom View
+                        if let images = imageFetcher.fetchedImages{
+                            ScrollView{
+                                CompositionalView(items: images, id: \.id) { item in
+            //                        ZStack {
+            //                            Rectangle()
+            //                                .fill(.cyan)
+            //                            Text("\(item)")
+            //                                .font(.title.bold())
+            //                        }
+                                    GeometryReader{ proxy in
+                                        let size = proxy.size
+                                        WebImage(url: URL(string: item.download_url))
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: size.width, height: size.height)
+                                            .cornerRadius(10)
+                                            .onAppear {
+                                                if images.last?.id == item.id {
+                                                    imageFetcher.startPagination = true
+                                                }
+                                            }
                                     }
                                 }
+                                .padding()
+                                .padding(.bottom, 10)
+                                
+                                if imageFetcher.startPagination && !imageFetcher.endPagination {
+                                    ProgressView()
+                                        .offset(y: -15)
+                                        .onAppear{
+                                            // MARK: Slight Delay
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                                imageFetcher.updateImages()
+                                            }
+                                        }
+                                }
+                            }
+                        } else {
+                            ProgressView()
                         }
                     }
-                } else {
-                    ProgressView()
+                    .navigationTitle("Compositional Layout")
                 }
+                    .tag(Tab.bookmark)
+                Text("Time")
+                    .applyBG()
+                    .tag(Tab.time)
+                Text("Camera")
+                    .applyBG()
+                    .tag(Tab.camera)
+                Text("Chat")
+                    .applyBG()
+                    .tag(Tab.chat)
+                Text("Settings")
+                    .applyBG()
+                    .tag(Tab.settings)
             }
-            .navigationTitle("Compositional Layout")
+            CustomTabbar(currentTab: $currentTab)
         }
     }
 }
@@ -64,5 +88,16 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func applyBG()-> some View{
+        self
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background{
+                Color("BG")
+                    .ignoresSafeArea()
+            }
     }
 }
---
+++ CompositionalLayout/Model/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  CompositionalLayout
+//
+//  Created by Claudius Kockelmann on 07.05.22.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case bookmark = "Bookmark"
+    case time = "Time"
+    case camera = "Camera"
+    case chat = "Chat"
+    case settings = "Settings"
+}
